da2494d87cf3b6b178e2c8c89ddf8f00a434f500
Sanya <troay89@mail.ru>
2020-06-20 17:35:12 +0300

    3 15 fix servlet

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -22,4 +23,13 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    meal_id INTEGER PRIMARY KEY,
+    dateTime date NOT NULL ,
+    description VARCHAR NOT NULL ,
+    calories INTEGER NOT NULL ,
+    FOREIGN KEY (meal_id) REFERENCES users (id) ON DELETE CASCADE
 );
